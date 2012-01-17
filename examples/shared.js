9565b85177488ec73ecb638c4479fc9dd6514f0f
James Halliday <mail@substack.net>
2012-01-17 02:15:23 -0800

    simple shared example

--- examples/shared.js
+++ examples/shared.js
@@ -1,13 +1,4 @@
-waitlist
-========
-
-Manage consumers standing in a queue for limited resources.
-
-example
-=======
-
-``` js
-var waitlist = require('waitlist');
+var waitlist = require('../');
 var EventEmitter = require('events').EventEmitter;
 
 var ws = waitlist();
@@ -37,44 +28,3 @@ ws.add('boop', { id : 1 });
         consume(userId + 1);
     }, Math.random() * 500);
 })(0)
-```
-
-output:
-
-```
-$ node examples/shared.js 
-user 0 using resource {"id":0}
-user 1 using resource {"id":1}
-user 2 in spot 1
-user 2 in spot 1
-user 3 in spot 2
-user 0 expired
-user 2 using resource {"id":0}
-user 3 in spot 1
-user 3 in spot 1
-user 4 in spot 2
-user 3 in spot 1
-user 4 in spot 2
-user 5 in spot 3
-user 1 expired
-user 3 using resource {"id":1}
-user 4 in spot 1
-user 5 in spot 2
-^C
-```
-
-methods
-=======
-
-
-
-install
-=======
-
-With [npm](http://npmjs.org) do:
-
-```
-npm install waitlist
-```
-
-[![build status](https://secure.travis-ci.org/substack/node-waitlist.png)](http://travis-ci.org/substack/node-waitlist)
