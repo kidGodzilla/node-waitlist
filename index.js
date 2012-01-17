4093f744eaa14bec27919016db3c40f2dda9038e
James Halliday <mail@substack.net>
2012-01-17 01:28:36 -0800

    prototype style

--- index.js
+++ index.js
@@ -1,121 +1,129 @@
 var EventEmitter = require('events').EventEmitter;
-var Hash = require('hashish');
+var hash = require('hashish');
 
-module.exports = Resources;
-Resources.prototype = new EventEmitter;
-function Resources () {
-    if (!(this instanceof Resources)) return new Resources();
-    var self = this;
-    var resources = self.resources = {};
-    
-    var queue = self.queue = [];
-    self.using = 0;
-    
-    self.stats = function () {
-        return {
-            resources : Object.keys(resources).length,
-            waiting : queue.length,
-            using : self.using,
-        };
-    };
-    
-    self.add = function (key, res) {
-        resources[key] = {
-            key : key,
-            resource : res,
-            lease : null,
-            emit : null,
-        };
-        self.emit('stats', self.stats());
-    };
-    
-    self.remove = function (key) {
-        delete resources[key];
-        self.emit('stats', self.stats());
-    };
-    
-    var tokens = {};
-    var emitters = {};
-    
-    self.acquire = function (ms, emit) {
-        do {
-            var token = Math.floor(Math.random() * Math.pow(2,32)).toString(16);
-        } while (tokens[token]);
-        tokens[token] = true;
-        
-        emit('token', token);
-        
-        var avail = Hash(resources).filter(function (res) {
-            return res.lease === null;
-        });
-        
-        if (avail.length > 0) {
-            dispatch(avail.values[0], token, ms, emit);
-        }
-        else {
-            queue.push({ token : token, time : ms, emit : emit });
-            queue.forEach(function (x, i) {
-                x.emit('spot', i + 1, queue.length);
-            });
-            self.emit('stats', self.stats());
-        }
-        
-        return token;
-    };
-    
-    self.release = function (token) {
-        delete tokens[token];
-        
-        var i = queue.map(function (q) { return q.token }).indexOf(token);
-        if (i >= 0) { // in the queue
-            queue.splice(i, 1);
-            queue.forEach(function (x, j) {
-                x.emit('spot', j + 1, queue.length);
-            });
-            self.emit('stats', self.stats());
-            return;
-        }
-        
-        var res = Hash(resources).detect(function (r) {
-            return r.lease && r.lease.token == token
-        });
-        
-        if (res) { // bound to a resource
-            res.emit('release');
-            res.lease = null;
-            res.emit = null;
-            
-            self.using -= 1;
-            
-            var q = queue.shift();
-            if (q) {
-                dispatch(res, q.token, q.time, q.emit);
-                queue.forEach(function (x, i) {
-                    x.emit('spot', i + 1, queue.length);
-                });
-            }
-            self.emit('stats', self.stats());
-        }
-    };
-    
-    function dispatch (res, token, ms, emit) {
-        res.lease = {
-            start : Date.now(),
-            time : ms,
-            token : token,
-        };
-        res.lease.end = res.lease.start + ms;
-        res.emit = emit;
-        
-        if (ms > 0) {
-            setTimeout(function () {
-                emit('expire');
-                self.release(token);
-            }, ms);
-        }
-        
-        emit('available', res.resource, res.key, res.lease);
-        self.using += 1;
-        self.emit('stats', self.stats());
-    }
+module.exports = function () {
+    return new Resources;
 }
+Resources.prototype = new EventEmitter;
+
+function Resources () {
+    this.resources = {};
+    this.queue = [];
+    this.sessions = {};
+    
+    this.stats = {
+        resources : 0,
+        waiting : 0,
+        using : 0
+    };
+}
+
+Resources.prototype.add = function (key, res) {
+    this.resources[key] = {
+        key : key,
+        resource : res,
+        lease : null,
+        emit : null,
+    };
+    this.stats.resources ++;
+    this.emit('stats', this.stats);
+};
+
+Resources.prototype.remove = function (key) {
+    delete this.resources[key];
+    this.emit('stats', this.stats);
+};
+
+Resources.prototype.acquire = function (ms, emit) {
+    var self = this;
+    
+    var token;
+    do {
+        token = Math.floor(Math.random() * Math.pow(2,32)).toString(16);
+    } while (self.sessions[token]);
+    
+    self.sessions[token] = { emit : emit, time : ms };
+    
+    emit('token', token);
+    
+    var avail = Object.keys(self.resources).filter(function (key) {
+        return self.resources[key].lease === null;
+    });
+    
+    if (avail.length > 0) {
+        self.dispatch(token, avail.values[0]);
+    }
+    else {
+        self.queue.push(token);
+        self.queue.forEach(function (id, i) {
+            self.sessions[id].emit('spot', i + 1, self.queue.length);
+        });
+        self.emit('stats', self.stats);
+    }
+    
+    return token;
+};
+
+Resources.prototype.release = function (token) {
+    var self = this;
+    delete self.tokens[token];
+    
+    var ix = self.queue.indexOf(token);
+    if (ix >= 0) {
+        queue.splice(ix, 1);
+        
+        queue.forEach(function (id, j) {
+            self.sessions[id].emit('spot', j + 1, self.queue.length);
+        });
+        
+        self.stats.waiting -= 1;
+        self.emit('stats', self.stats);
+        
+        return;
+    }
+    
+    var res = hash(resources).detect(function (r) {
+        return r.lease && r.lease.token == token
+    });
+    
+    if (res) { // bound to a resource
+        res.emit('release');
+        res.lease = null;
+        res.emit = null;
+        
+        self.stats.using -= 1;
+        
+        var q = queue.shift();
+        if (q) {
+            self.dispatch(q.token, res);
+            self.queue.forEach(function (id, i) {
+                self.sessions[id].emit('spot', i + 1, self.queue.length);
+            });
+        }
+        self.emit('stats', self.stats);
+    }
+};
+
+Resources.prototype.dispatch = function (token, res) {
+    var self = this;
+    var session = self.sessions[token];
+    
+    res.lease = {
+        start : Date.now(),
+        time : session.time,
+        token : token,
+    };
+    res.lease.end = res.lease.start + ms;
+    res.emit = session.emit;
+    
+    if (session.time > 0) {
+        setTimeout(function () {
+            emit('expire');
+            self.release(token);
+        }, session.time);
+    }
+    
+    session.emit('available', res.resource, res.key, res.lease);
+    self.stats.using += 1;
+    self.emit('stats', self.stats);
+};
